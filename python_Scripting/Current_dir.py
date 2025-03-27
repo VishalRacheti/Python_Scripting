66dd43c7c5d2847c2d1354ef59a1c8130b667c60
Vishal Racheti <vishal.racheti6080@gmail.com>
2025-03-27 23:09:41 +0530

    first commit

---
+++ python_Scripting/Current_dir.py
@@ -0,0 +1,7 @@
+import os
+
+print ("To Get current working directory", os.getcwd()) # To get current working directory
+
+os.chdir("C:/Users/dell/Downloads") # To change current working directory
+
+print (" Current Working directory", os.getcwd()) # To working directory
